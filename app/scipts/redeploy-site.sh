c13f53b3421ee9d78456d4ee6431dbe38b53468f
[LauraAurora] <[laura.apenza@gmail.com]>
2023-02-17 12:54:35 -0500

    scripts

--- app/scipts/redeploy-site.sh
+++ app/scipts/redeploy-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Reset environment and update project.
-tmux kill-server;
-cd "~/repos/project-placeholder";
-git stash && git switch main && git fetch --all && git reset --hard origin/main;
+tmux kill-server
+cd "~/project-placeholder"
+git fetch && git reset origin/main --hard
 
 # Boot process.
-tmux new-session -d -s process "cd ~/repos/project-placeholder && source venv/bin/activate && pip install -q -r requirements.txt && flask run --host=0.0.0.0";
+tmux new-session -d -s process "cd ~/project-placeholder && source venv/bin/activate && pip install -q -r requirements.txt && flask run --host=0.0.0.0";
