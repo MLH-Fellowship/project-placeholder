89f5463ba6a54619457b63971252488132279643
ferdeleong <ferdeleong>
2023-02-23 19:06:40 +0000

    Add environment variables

--- app/scipts/redeploy-site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Reset environment and update project.
-tmux kill-server
-cd "~/project-placeholder"
-git fetch && git reset origin/main --hard
-
-# Boot process.
-tmux new-session -d -s process "cd ~/project-placeholder && source venv/bin/activate && pip install -q -r requirements.txt && flask run --host=0.0.0.0";
