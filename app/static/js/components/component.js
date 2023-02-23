b260f040fefd8922e3125063b1fc450ce333a760
dmosc <40501486+dmosc@users.noreply.github.com>
2023-02-22 18:41:24 -0600

    Revert "Implement timeline section with creation and retrieval support of Posts."
    This reverts commit 8ed2b4c255224ecac63ff4e43185ddece369103b.

--- app/static/js/components/component.js
+++
@@ -1,9 +0,0 @@
-class Component {
-  constructor() {}
-
-  build(...args) {
-    throw new Error(`Method not implemented; passed args: ${args}`);
-  }
-}
-
-export {Component};
--- app/static/js/request-manager.js
+++
@@ -1,9 +0,0 @@
-class RequestManager {
-  static async getPosts() {
-    const payload = await fetch("/api/get_posts");
-    const posts = await payload.json();
-    return posts;
-  }
-}
-
-export {RequestManager};
