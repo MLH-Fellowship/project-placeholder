1a8a8abf1c1def53b618c1d8db521d43947e6b6d
Fernanda De León <78885738+ferdeleong@users.noreply.github.com>
2023-02-25 22:38:34 -0600

    Revert "Timeline posts 01 (#43)"
    This reverts commit 1a9fd0a08ce909f6a8f7d541b5bd16a58ef4713c.

--- app/scripts/curl-test.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'name=test&email=test&content=test' http://localhost:5000/api/create_timeline_post
-curl -X GET http://localhost:5000/api/get_timeline_post | jq '.timeline_posts[0]'
