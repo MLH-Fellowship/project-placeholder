5b5fe116144e87e696ea11757054202a8a3a7510
Anshaal Hussain <anshaalhussain@gmail.com>
2023-03-08 13:54:03 -0500

    change mysql password peewee

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -12,3 +12,17 @@ git stash && git switch main && git fetch --all && git reset --hard origin/main;
 # Start a new detached Tmux session that goes to the project directory, enters the python virtual environment and starts the Flask server.
 
 tmux new-session -d -s process "cd ~/repos/project-placeholder && source venv/bin/activate && pip install -q -r requirements.txt && flask run --host=0.0.0.0";
+
+# root@146.190.240.33
+
+# tmux new
+
+# python -m ven python3-virutalenv
+
+# source python3-virtualenv/bin/activate
+
+# pip install -r requirements.txt
+
+# flask run --host=0.0.0.0
+
+# cmd-B + D
