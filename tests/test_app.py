ddfe2e6ebd7e221cb5ed2c775d174a0211c232af
Anshaal Hussain <anshaalhussain@gmail.com>
2023-04-03 14:03:46 -0400

    fix test issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,26 @@
+from app import app
 import unittest
 import os
 os.environ['TESTING'] = 'true'
 
-from app import app
 
 class AppTestCase(unittest.TestCase):
-    def seUp(self):
+    def setUp(self):
         self.client = app.test_client()
-    
+
     def test_home(self):
         response = self.client.get("/")
         assert response.status_code == 200
         html = response.get_data(as_text=True)
-        assert "<title>MLH Fellow</title>" in html
+        assert "<title>Week 1 - Team Portfolio</title>" in html
         # TODO Add more tests relating to the home page
-      
+
     def test_timeline(self):
         response = self.client.get("/api/timeline_post")
         assert response.status_code == 200
-        assert response.ls_json
+        assert response.is_json
         json = response.get_json()
         assert "timeline_posts" in json
-        assert len(json["timeline_posts"]) == 0
+        assert len(json["timeline_posts"]) == 9
         # TODO Add more tests relating to the /api/timeline_post GET and POST apis
         # TODO Add more tests relating to the timeline page
